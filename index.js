a4777c56d695464d1a901ead478cf63f85f5135b
crash <yashwanthkrishna5027@gmail.com>
2021-09-25 08:06:24 +0530

    responsive again

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ navlinks.forEach(link => {
 
 //SCROLL
 
-const vh = document.documentElement.clientHeight 
+const vh = document.documentElement.clientHeight ;
 var height=0;
 var body = document.body,
     html = document.documentElement;
@@ -32,63 +32,7 @@ $(document).on('mousewheel DOMMouseScroll', function (event) {
     }
 });
 
-//                        window.addEventListener('wheel', function(e) {
-//     direction = e.deltaY
 
-//     if (direction > 0) {
-//         console.log('scrolling up')
-//                next();
-//     }else{
-//         console.log('scrolling down')
-//         prev();
- 
-//     }
-// })
-// $(window).on('mousewheel DOMMouseScroll', function(e){
-//     if(e.originalEvent.detail > 0) {
-//         //scroll down
-//      next();
-//     }else {
-//         prev();
-//         //scroll up
-//         // $('.slide-1').addClass('active');
-//         // $('.slide-2').removeClass('active');
-//     }
-//     //prevent page fom scrolling
-//     return false;
-// });
-// var scroll=$(document).scrollTop();
-
-// $(window).scroll(function(){
-//     var scrolled=$(document).scrollTop();
-//     if(scrolled>scroll){
-//     next();
-    
-//     }else{
-// prev();
-
-//     }
-// scroll=$(document).scrollTop();
-// })
-
-//  var lastScrollTop = 0, delta = 5;
-// 	 $(window).scroll(function(){
-// 		 var nowScrollTop = $(this).scrollTop();
-// 		 if(Math.abs(lastScrollTop - nowScrollTop) >= delta){
-// 		 	if (nowScrollTop > lastScrollTop){
-// 		 		// ACTION ON
-// 		 		// SCROLLING DOWN 
-//                  next();
-//                 sleep(2000);
-// 		 	} else {
-// 		 		// ACTION ON
-// 		 		// SCROLLING UP 
-//                  prev();
-// sleep(2000)
-// 			}
-// 		 lastScrollTop = nowScrollTop;
-// 		 }
-// 	 });
 
 function next(){
     if(height<max){
@@ -111,3 +55,6 @@ function sleep(milliseconds) {
     currentDate = Date.now();
   } while (currentDate - date < milliseconds);
 }
+
+
+
